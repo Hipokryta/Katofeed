6a62396f96970a3f806f4d94dac42a217c19a3ee
hipokryta <shugo12345@gmail.com>
2014-11-22 16:14:41 +0100

    gotowe

--- katofeed/app/scripts/controllers/main.js
+++ katofeed/app/scripts/controllers/main.js
@@ -10,13 +10,8 @@
 angular.module('noweDzieloApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.imagesArray = [];
-   //  $scope.imagesArray[0] = {image:'images/1.jpg',desc:'DUPA'};
-   //  $scope.imagesArray[1] = {image:'images/COMA.jpg',desc:'poqpeowqp'};
-   //  $scope.imagesArray[2] = {image:'images/images (2).jpg', desc:'4tttt'};
-  	// $scope.imagesArray[3] = {image:'images/images.jpg',desc:'jjdvlkjvd'};
 	
-	$scope.changingStyle = null;
-	$scope.endOfSite = 10;
+	
  
 	$http.get('getfbstream.php')
             .success(function(res) {
